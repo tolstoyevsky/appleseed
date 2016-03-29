1c7dcfb20802d470d24c3aa6a48cfb20317260c0
Evgeny Golyshev <grga-pitic@yandex.ru>
2016-03-29 18:11:41 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='appleseed',
+      version='0.1',
+      description='',
+      url='https://bitbucket.org/eugulixes/appleseed',
+      author='CusDeb Team',
+      maintainer='Evgeny Golyshev',
+      maintainer_email='Evgeny Golyshev <eugulixes@gmail.com>',
+      license='http://www.apache.org/licenses/LICENSE-2.0',
+      scripts=['bin/appleseed.py'],
+      install_requires=[
+          'chardet',
+          'pymongo',
+          'python-debian',
+          'tornado'
+      ])
