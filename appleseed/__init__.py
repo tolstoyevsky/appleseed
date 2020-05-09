14b15ebb3608e54cbe2e99a46c32581a48f68e87
Evgeny Golyshev <eugulixes@gmail.com>
2020-05-10 00:44:55 +0300

    Move logic related to downloading an decompressing to IndexFile
    The code related to Alpine was removed as a side effect.

--- appleseed/__init__.py
+++ appleseed/__init__.py
@@ -1,12 +1,18 @@
+import gzip
+import lzma
+import os
+import os.path
 import re
+import shutil
+import urllib.parse
+import urllib.request
+import uuid
 
 from appleseed import apt_pkg
 from appleseed.deb822 import Deb822, Deb822Dict, TagSectionWrapper
 
 
-ALPINE = 0
-
-DEBIAN_BASED = 1
+ALLOWED_DISTROS = ('debian', 'devuan', 'raspbian', 'kali', 'ubuntu', )
 
 _MAP = {
     'A': 'Architecture',
@@ -20,35 +26,66 @@ _MAP = {
 }
 
 
+class UnknownDistro(Exception):
+    pass
+
+
 class IndexFile:
-    def __init__(self, sequence, distro):
-        self._sequence = sequence
+    def __init__(self, distro, suite, section, arch, mirror, parent_temp_dir='/tmp'):
+        self._parent_temp_dir = parent_temp_dir
+        self._temp_dir = None
 
-        if distro == ALPINE:
-            self.iter_paragraphs = self._go_through_alpine_index_file()
-        elif distro == DEBIAN_BASED:
-            self.iter_paragraphs = self._go_through_debian_index_file()
+        if distro not in ALLOWED_DISTROS:
+            raise UnknownDistro
 
-    def _go_through_alpine_index_file(self):
-        paragraph = Deb822Dict()
-        for line in self._sequence.readlines():
-            if line == '\n':
-                yield paragraph
-                paragraph = Deb822Dict()
-                continue
+        uri = f'dists/{suite}/{section}/binary-{arch}/Packages'
 
-            _empty, key, val = re.split(r'^(\w):', line, flags=re.IGNORECASE)
+        self._index_file_path = None  # will be known later
+        self._url = urllib.parse.urljoin(mirror, uri)
 
-            try:
-                key = _MAP[key]
-            except KeyError:
-                pass
+    def get_url(self):
+        raise NotImplemented
 
-            paragraph[key] = val
+    def iter_paragraphs(self):
+        raise NotImplemented
 
-    def _go_through_debian_index_file(self):
-        parser = apt_pkg.TagFile(self._sequence, bytes=False)
-        for section in parser:
-            paragraph = Deb822(_parsed=TagSectionWrapper(section))
-            if paragraph:
-                yield paragraph
+    def download(self):
+        self._index_file_path = os.path.join(self._temp_dir, os.path.basename(self._url))
+        with urllib.request.urlopen(self._url) as response:
+            with open(self._index_file_path, 'b+w') as outfile:
+                outfile.write(response.read())
+
+    def __enter__(self):
+        self._temp_dir = os.path.join(self._parent_temp_dir, str(uuid.uuid4()))
+        os.mkdir(self._temp_dir, mode=0o700)
+
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        shutil.rmtree(self._temp_dir)
+
+
+class DebianIndexFile(IndexFile):
+    def __init__(self, *args, **kwargs):
+        self._debian_packages_ext = {'.xz': lzma.open, '.gz': gzip.open, }
+        self._ext = None  # index file extension (one of _debian_packages_ext)
+
+        super().__init__(*args, **kwargs)
+
+    def get_url(self):
+        url_bck = self._url
+        for self._ext in list(self._debian_packages_ext.keys()) + ['']:
+            # An empty list means an uncompressed index file.
+
+            self._url = url_bck + self._ext
+            yield self._url
+
+    def iter_paragraphs(self):
+        func = self._debian_packages_ext[self._ext] if self._ext else open
+        kwargs = {'encoding': 'utf-8'} if func == open else {}
+        with func(self._index_file_path, **kwargs) as infile:
+            parser = apt_pkg.TagFile(infile, bytes=False)
+            for section in parser:
+                paragraph = Deb822(_parsed=TagSectionWrapper(section))
+                if paragraph:
+                    yield paragraph
